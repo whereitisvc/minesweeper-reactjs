31fc54863da80cff6908c76f462fa4db84a7ddeb
Nai-Jack Li <whereitisvc@gmail.com>
2018-11-23 17:03:00 -0800

    Updates

---
+++ precache-manifest.5fbb5c30b2386ad4b1cf0555dbde1e80.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e18c7b9be89e2542d604fafac0390e",
+    "url": "/minesweeper-reactjs/static/media/stage2.05e18c7b.png"
+  },
+  {
+    "revision": "dc4541117db7a2c080b3",
+    "url": "/minesweeper-reactjs/static/css/main.c9a5a3d1.chunk.css"
+  },
+  {
+    "revision": "b504042478849d2e0b3e47a397d062ef",
+    "url": "/minesweeper-reactjs/static/media/close1.b5040424.png"
+  },
+  {
+    "revision": "463b5f6bd1b7fd4c8e7e",
+    "url": "/minesweeper-reactjs/static/js/1.463b5f6b.chunk.js"
+  },
+  {
+    "revision": "3edb01d5a064d0952d29",
+    "url": "/minesweeper-reactjs/static/js/runtime~main.3edb01d5.js"
+  },
+  {
+    "revision": "df9b055420d8ff63cb0101a3731de31a",
+    "url": "/minesweeper-reactjs/static/media/flag.df9b0554.svg"
+  },
+  {
+    "revision": "4fe15c639be55a8c9af21ba857aca436",
+    "url": "/minesweeper-reactjs/static/media/New.4fe15c63.svg"
+  },
+  {
+    "revision": "8c0db196328cdb1e240a66301afb91c5",
+    "url": "/minesweeper-reactjs/static/media/Undo.8c0db196.svg"
+  },
+  {
+    "revision": "ecc7e14fd525cb5659a11b0906e04e9f",
+    "url": "/minesweeper-reactjs/static/media/Redo.ecc7e14f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minesweeper-reactjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "20ecba2cfa792234dbaa7d72c92e4641",
+    "url": "/minesweeper-reactjs/static/media/term.20ecba2c.png"
+  },
+  {
+    "revision": "dc4541117db7a2c080b3",
+    "url": "/minesweeper-reactjs/static/js/main.dc454111.chunk.js"
+  },
+  {
+    "revision": "8e3fa3075704668d7b3600f35c77fc60",
+    "url": "/minesweeper-reactjs/static/media/stage3a.8e3fa307.png"
+  },
+  {
+    "revision": "94dabd194419a0b4ee78e26613c51336",
+    "url": "/minesweeper-reactjs/static/media/stage3b.94dabd19.png"
+  },
+  {
+    "revision": "788a5710d425ebda4507d1845a4db6ce",
+    "url": "/minesweeper-reactjs/static/media/stage3c.788a5710.png"
+  },
+  {
+    "revision": "f9a616834b67bfd4df9283c54dbc3cfb",
+    "url": "/minesweeper-reactjs/static/media/stage4a.f9a61683.png"
+  },
+  {
+    "revision": "58a90ad47dce3b7ee4981166625e6449",
+    "url": "/minesweeper-reactjs/static/media/stage4b.58a90ad4.png"
+  },
+  {
+    "revision": "04fda8e2e195b8782db9cd9c8bae1e6f",
+    "url": "/minesweeper-reactjs/static/media/segment.04fda8e2.png"
+  },
+  {
+    "revision": "ecb287c7b6f8209a621da816bda4c246",
+    "url": "/minesweeper-reactjs/static/media/close2.ecb287c7.png"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/minesweeper-reactjs/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "463b5f6bd1b7fd4c8e7e",
+    "url": "/minesweeper-reactjs/static/css/1.6626a50c.chunk.css"
+  },
+  {
+    "revision": "ba97686d0ddb41f36edb04a4e3746378",
+    "url": "/minesweeper-reactjs/index.html"
+  }
+];
